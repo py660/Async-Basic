55eaa5f5d05556a60e9dfc4e4528f96d02d12812
py660 <93660666+py660@users.noreply.github.com>
2023-07-06 18:33:00 +0800

    add install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+which python3 pip3 > /dev/null || {echo "Python3 is required. Please install it, then re-run this script."; exit 1}
 pip3 install --upgrade poetry
 python3 -m poetry install
 python3 -m poetry lock
